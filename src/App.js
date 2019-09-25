2ddbbd59ccfc35ca123674144454d3c7f650168e
Elyse Burmester <burmesterelyse@gmail.com>
2019-09-25 17:01:13 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -14,7 +14,17 @@ class App extends Component {
     this.handleAddTimer()
   }
 
+<<<<<<< HEAD
  render() {
+=======
+
+
+
+
+  // No need to modify anything in render or the class methods below
+  // Unless, of course, you're curious about how it all works
+  render() {
+>>>>>>> aeb9415175aba547e59c38784dc0e407613f9dd2
 
     return (
       <div className="App">
@@ -29,19 +39,29 @@ class App extends Component {
     );
   }
 
+<<<<<<< HEAD
   // returns array of components written in JSX, mapped from this.state.timerIDs
+=======
+>>>>>>> aeb9415175aba547e59c38784dc0e407613f9dd2
   renderTimers = () => this.state.timerIDs.map(id => {
     return <Timer key={id} id={id} removeTimer={this.removeTimer} />
   })
 
+<<<<<<< HEAD
   // adds a random number for timer ID
+=======
+ 
+>>>>>>> aeb9415175aba547e59c38784dc0e407613f9dd2
   handleAddTimer = () => {
     this.setState(prevState => ({
       timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
     }))
   }
 
+<<<<<<< HEAD
   // removeTimer updates state, removing any timer that matches the provided author
+=======
+>>>>>>> aeb9415175aba547e59c38784dc0e407613f9dd2
   removeTimer = id => {
     this.setState(prevState => ({
       timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
